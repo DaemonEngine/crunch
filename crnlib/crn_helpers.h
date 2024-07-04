28d9bc589b0967220dc712cb98407b7f07ec804f
Thomas Debesse <dev@illwieckz.net>
2024-07-04 03:17:08 +0200

    crnlib: fix -Wpedantic: extra ‘;’ in macro

--- crnlib/crn_helpers.h
+++ crnlib/crn_helpers.h
@@ -4,7 +4,7 @@
 
 #define CRNLIB_NO_COPY_OR_ASSIGNMENT_OP(c) \
   c(const c&);                             \
-  c& operator=(const c&);
+  c& operator=(const c&)
 #define CRNLIB_NO_HEAP_ALLOC()       \
  private:                            \
   static void* operator new(size_t); \
